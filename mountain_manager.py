0a34680bde71890d5a51b45fa9bbfd348974c069
Vman384 <112357168+Vman384@users.noreply.github.com>
2023-04-15 18:52:27 +1000

    passing all tests

--- mountain_manager.py
+++ mountain_manager.py
@@ -15,7 +15,7 @@ class MountainManager:
         '''
         Remove a mountain from the manager
         '''
-        self.mountain_store[mountain.name] = None
+        self.mountain_store.__delitem__(mountain.name)
 
     def edit_mountain(self, old: Mountain, new: Mountain):
         '''
@@ -39,4 +39,32 @@ class MountainManager:
         '''
         Returns a list of lists of all mountains, grouped by and sorted by ascending difficulty.
         '''
-        raise NotImplementedError()
+        self.grouped = []
+        self.temp_store = self.mountain_store.values()
+        for index in range(len(self.temp_store)):
+            if self.temp_store[index] == None:
+                pass
+            else:
+                self.current_difficulty_list = [self.temp_store[index]]
+                self.current_difficulty = self.temp_store[index].difficulty_level
+                self.temp_store[index] = None
+                for index_to_compare in range(len(self.temp_store)):
+                    if self.temp_store[index_to_compare] == None:
+                        pass
+                    elif self.temp_store[index_to_compare].difficulty_level == self.current_difficulty:
+                        self.current_difficulty_list.append(self.temp_store[index_to_compare])
+                        self.temp_store[index_to_compare] = None
+                if len(self.grouped) == 0:
+                    self.grouped.append(self.current_difficulty_list)
+                else:
+                    for i in range(len(self.grouped)):
+                        if self.grouped[i][0].difficulty_level > self.current_difficulty:
+                            self.temp = self.grouped[i]
+                            self.grouped[i] = self.current_difficulty_list
+                            self.current_difficulty_list = self.temp
+                            self.current_difficulty = self.current_difficulty_list[0].difficulty_level
+                    if self.grouped[-1] != self.current_difficulty_list:    
+                        self.grouped.append(self.current_difficulty_list)
+        return self.grouped
+
+
